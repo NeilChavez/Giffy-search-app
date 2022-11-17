26f27427017b672c7643523820943421e38139ca
NeilChavez <lolochavez22@icloud.com>
2022-11-17 19:20:17 +0100

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+<<<<<<< HEAD
 import "./App.css";
 
+=======
+import "./App.css"
+>>>>>>> a36c600 (Add deployment on vercel)
 
 function App() {
   const [gifs, setGifs] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIFFY_APY_KEY}&q=&limit=25&offset=0&rating=g&lang=en`
-    )
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  }, []);
-
-  return (
-    <div className="App-content">
-      <Link to="/">Logo</Link>
-    </div>
-  );
+  // useEffect(() => {
+  //   fetch(
+  //     // `https://api.giphy.com/v1/gifs/search?api_key=qFJI66lO4rOfZiiOV8a9XlmyMYOrLJfF&q=&limit=25&offset=0&rating=g&lang=en`
+  //   )
+  //     // .then((res) => res.json())
+  //     // .then((json) => console.log(json));
+  // }, []);
+  console.log(process.env);
+  return <div className="App-content">{process.env.GIFFY_APY_KEY}</div>;
 }
 
 export default App;
