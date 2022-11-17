15deb02807e78e8624861d04fdcecb8f939048b3
NeilChavez <lolochavez22@icloud.com>
2022-11-17 21:54:29 +0100

    Add search form and search functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import "./App.css";
+import { useState } from "react";
 import Gif from "./components/Gif";
 import { useGifs } from "./hooks/useGifs";
+import "./App.css";
 
 function App() {
-const {gifs, loading} = useGifs();
+  const [keyword, setKeyword] = useState("");
+  const [keywordToSearch, setKeyWorkToSearch] = useState("");
+  const { gifs, loading } = useGifs({ keyword: keywordToSearch });
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (keyword) {
+      alert("send the form");
+      setKeyWorkToSearch(keyword);
+    } else {
+      //TODO getisci l'errore del fomulario
+      alert("you need to fill the form");
+    }
+  };
 
   return (
     <div>
-      {gifs.map(({ id, urlGif, title }) => (
-        <Gif key={id} id={id} urlGif={urlGif} title={title} />
-      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search a gif..."
+          onChange={handleChange}
+          value={keyword}
+        />
+      </form>
+      {loading ? (
+        <p>LOADING...</p>
+      ) : (
+        gifs.map(({ id, urlGif, title }) => (
+          <Gif key={id} id={id} urlGif={urlGif} title={title} />
+        ))
+      )}
     </div>
   );
 }
--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,17 +2,18 @@ import { useState, useEffect } from "react";
 import { getGifs } from "../services/getGifs";
 import { API_KEY, API_URL } from "../settings/settings";
 
-export function useGifs() {
+export function useGifs({keyword}) {
   const [loading, setLoading] = useState(false);
   const [gifs, setGifs] = useState([]);
-
+  
   useEffect(() => {
-    const endpoint = `${API_URL}/gifs/search?api_key=${API_KEY}&q=panda&limit=25&offset=0&rating=g&lang=en`;
+    const keywordToSearch = keyword || "Avengers"
+    const endpoint = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keywordToSearch}&limit=25&offset=0&rating=g&lang=en`;
     setLoading(true);
     getGifs(endpoint).then((res) => {
       setGifs(res);
       setLoading(false);
     });
-  }, []);
+  }, [keyword]);
   return { loading, gifs };
 }
