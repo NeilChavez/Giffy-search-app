1a4f38023b636c5db5a6a576cc776e7a0eef89af
NeilChavez <lolochavez22@icloud.com>
2022-11-17 17:44:59 +0100

    Add fetch to the api giphy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import "../";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIFFY_APY_KEY}&q=&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
 
   return (
     <div className="App-content">
