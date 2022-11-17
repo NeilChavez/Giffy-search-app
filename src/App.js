af503485ed2233fb8a1ae5825d12099d69aba2cc
NeilChavez <lolochavez22@icloud.com>
2022-11-17 17:41:34 +0100

    Remove components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { Link, Routes, Route } from "react-router-dom";
+import {useEffect, useState} from "react"
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App-content">
-      <Link to="/">
-        Logo
-      </Link>
-
-      <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/search/:keyword" element={<div>Search keyword</div>} />
-        <Route path="/gif/:id" element={<div>show a gif</div>} />
-      </Routes>
+      <Link to="/">Logo</Link>
     </div>
   );
 }
