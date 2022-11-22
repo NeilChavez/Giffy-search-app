b8cd9bc814b91339ef383e1f99c35bf4665a1543
NeilChavez <lolochavez22@icloud.com>
2022-11-22 16:46:41 +0100

    Add useReducer to have more control over the keyword and rating state

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,21 +1,55 @@
-import { memo, useState } from "react";
+import { memo, useState, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import { RATINGS } from "../../settings/settings";
 
+const TYPES = {
+  UPDATE_KEYWORD: "update-keyword",
+  UPDATE_RATING: "update-rating",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case TYPES.UPDATE_KEYWORD: {
+      return {
+        ...state,
+        keyword: action.payload,
+      };
+    }
+    case TYPES.UPDATE_RATING:
+      return {
+        ...state,
+        rating: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 function SearchForm({ initialKeyword = "", initialRating = RATINGS[0] }) {
-  const [keyword, setKeyword] = useState(initialKeyword);
-  const [rating, setRating] = useState(initialRating);
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setKeyword(e.target.value);
+  // const [keyword, setKeyword] = useState(initialKeyword);
+  // const [rating, setRating] = useState(initialRating);
+  const initialState = {
+    keyword: initialKeyword,
+    rating: initialRating,
   };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { keyword, rating } = state;
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    dispatch({ type: TYPES.UPDATE_KEYWORD, payload: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/search/${keyword}/${rating}`);
   };
+
   const handleOnChange = (e) => {
-    setRating(e.target.value);
+    dispatch({ type: TYPES.UPDATE_RATING, payload: e.target.value });
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -24,6 +58,7 @@ function SearchForm({ initialKeyword = "", initialRating = RATINGS[0] }) {
         onChange={handleChange}
         value={keyword}
       />
+      {console.log(state)}
       <select onChange={handleOnChange} value={rating}>
         <option disabled>Select a rating</option>
         {RATINGS.map((rating) => (
