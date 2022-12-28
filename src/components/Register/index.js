ecb0969d74ae42172db09354bd632907696dbbcd
NeilChavez <lolochavez22@icloud.com>
2022-12-28 17:36:22 -0500

    Add formik to manage the Register Form Component

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,67 +1,99 @@
-import { useState } from "react";
-import { useAuth } from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-import "./Register.css";
-const initialState = {
-  email: "",
-  password: "",
-};
-export default function Register() {
-  const [form, setForm] = useState(initialState);
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
-  const { signUp } = useAuth();
+// import { useState } from "react";
+// import { useAuth } from "../../hooks/useAuth";
+// import { useNavigate } from "react-router-dom";
+// import "./Register.css";
+// const initialState = {
+//   email: "",
+//   password: "",
+// };
+// export default function Register() {
+//   const [form, setForm] = useState(initialState);
+//   const [error, setError] = useState(null);
 
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleSubmit = async (email, password) => {
-    //manda a navigation /home, se tutto va bene
-    try {
-      await signUp(email, password);
-      navigate("/");
-    } catch (err) {
-      console.warn(err);
-      setError(err);
-      console.warn("sono nel catch, qualcosa e' andato male");
-    }
-  };
+
+//   const navigate = useNavigate();
+//   const { signUp } = useAuth();
+
+//   const handleChange = (e) => {
+//     setForm({
+//       ...form,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+//   const handleSubmit = async (email, password) => {
+//     //manda a navigation /home, se tutto va bene
+//     try {
+//       await signUp(email, password);
+//       navigate("/");
+//     } catch (err) {
+//       console.warn(err);
+//       setError(err);
+//       console.warn("sono nel catch, qualcosa e' andato male");
+//     }
+//   };
+//   return (
+//     <>
+//       {error && (
+//         <div style={{ border: "thin solid red", display: "inline-block" }}>
+//           {error.code}
+//         </div>
+//       )}
+//       <div className="Form-wrapper">
+//         <h2>Register</h2>
+//         <form
+//           className="Form"
+//           onSubmit={(e) => {
+//             e.preventDefault();
+//             handleSubmit(form.email, form.password);
+//           }}
+//         >
+//           <input
+//             onChange={handleChange}
+//             name="email"
+//             type="email"
+//             placeholder="insert your mail"
+//             value={form.email}
+//           />
+//           <input
+//             onChange={handleChange}
+//             name="password"
+//             type="password"
+//             placeholder="insert your password"
+//             value={form.password}
+//           />
+//           <input type="submit" value="Register" />
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+import { Formik } from "formik";
+export default function Register() {
   return (
-    <>
-      {error && (
-        <div style={{ border: "thin solid red", display: "inline-block" }}>
-          {error.code}
-        </div>
-      )}
-      <div className="Form-wrapper">
-        <h2>Register</h2>
-        <form
-          className="Form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(form.email, form.password);
-          }}
-        >
-          <input
-            onChange={handleChange}
-            name="email"
-            type="email"
-            placeholder="insert your mail"
-            value={form.email}
-          />
-          <input
-            onChange={handleChange}
-            name="password"
-            type="password"
-            placeholder="insert your password"
-            value={form.password}
-          />
-          <input type="submit" value="Register" />
-        </form>
-      </div>
-    </>
-  );
+    <Formik
+      initialValues={{
+        username: "",
+        password: ""
+      }}
+      onSubmit={(values) => {
+        console.log(values)
+      }}>
+      {(formik) => <form onSubmit={formik.handleSubmit}>
+        <input
+          name="username"
+          id="username"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.username} />
+        <input
+          name="password"
+          id="password"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.password} />
+        <button type="submit" className="btn" disabled={formik.isSubmitting}>Registrate</button>
+      </form>}
+    </Formik>
+  )
 }
